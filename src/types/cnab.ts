e6065f7fbfd2aa63e6c356cb24cee7d8810b6656
onoriorafa <83240365+onoriorafa@users.noreply.github.com>
2026-03-18 02:59:39 -0300

    feat(validateCnabContent): add new validations

--- src/types/cnab.ts
+++ src/types/cnab.ts
@@ -8,13 +8,27 @@ export async function validateCnabContent(file: File) {
     throw new Error("O arquivo está vazio.");
   }
 
-  //verificar se todas as linhas têm o tamanho correto
+  // 1 - Validar o tamanho de cada linha (deve ser exatamente 444 caracteres)
   const invalidLines = lines.some((line) => line.length !== LINE_LENGTH);
 
   if (invalidLines) {
     throw new Error(
       `O arquivo CNAB não é válido. Cada linha deve conter exatamente ${LINE_LENGTH} caracteres.`,
     );
+  }
+
+  // 2 - Verificar se tem o Header (primeira linha)
+  const hasHeader = lines[0]?.startsWith("0");
+
+  if (!hasHeader) {
+    throw new Error("O arquivo CNAB não possui um header válido.");
+  }
+
+  // 3 - Verificar se tem o Trailer (última linha)
+  const hasTrailer = lines[lines.length - 1]?.startsWith("9");
+
+  if (!hasTrailer) {
+    throw new Error("O arquivo CNAB não possui um trailer válido.");
   }
 
   return lines;
